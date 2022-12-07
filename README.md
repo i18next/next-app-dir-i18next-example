461424d33bc68ce8b34f89e8a072651b64aaa7ba
Adriano Raiano <adriano@raiano.ch>
2022-12-07 22:45:17 +0100

    i18next cookie

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This example shows a basic way to use [i18next](https://www.i18next.com) (and [react-i18next](https://react.i18next.com)) in a Next.js app with the new app dir structure.
 [next-i18next](https://next.i18next.com) is not needed anymore for this setup.
 
-It has server side pages and some client side pages.
+It shows i18next integration on some server side pages and some client side pages.
+
+There is also an example middleware with language detection and persistence via cookie.
 
 *This example has been created out of [this discussion](https://github.com/i18next/next-i18next/discussions/1993).*
--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,27 @@ import { fallbackLng, languages } from './app/i18n/settings'
 acceptLanguage.languages(languages)
 
 export const config = {
-  matcher: ['/']
+  matcher: ['/'].concat(languages.map((l) => `/${l}/`))
 }
+
+const cookieName = 'i18next'
 
 export function middleware(req: NextRequest) {
   let lng
+  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
   if (!lng) lng = fallbackLng
 
   if (req.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL(`/${lng}`, req.url))
+  }
+
+  if (req.headers.has('referer')) {
+    const refererUrl = new URL(req.headers.get('referer'))
+    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
+    const response = NextResponse.next()
+    if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
+    return response
   }
 
   return NextResponse.next()
