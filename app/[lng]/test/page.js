b6e0fd78fc614919a6397693ab2664594845b6c5
David Knezić <davidknezic@gmail.com>
2022-12-07 16:30:34 +0100

    Turn test page into client component

--- app/[lng]/test/page.js
+++ app/[lng]/test/page.js
@@ -1,4 +1,6 @@
-import { use } from 'react'
+'use client';
+
+import { use, useState } from 'react'
 import i18next, { languages } from '../../i18n'
 
 export async function generateStaticParams() {
@@ -17,9 +19,21 @@ export async function generateStaticParams() {
 
 export default function Page({ params }) {
   const { t } = use(i18next(params.lng))
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
       <h1>{t('welcome')}</h1>
+      <p>{t('counter', { count: counter })}</p>
+      <div>
+        <button onClick={() => setCounter(Math.max(0, counter - 1))}>
+          decrement
+        </button>
+        <button onClick={() => setCounter(Math.min(3, counter + 1))}>
+          increment
+        </button>
+      </div>
     </>
   );
 }
