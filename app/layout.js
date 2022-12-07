121ebc42096875290dae0525fa1e6232efb6bf8b
Adriano Raiano <adriano@raiano.ch>
2022-12-07 08:47:54 +0100

    hint for html lang #1

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,15 @@
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }) {
+  // TODO: Here's a problem: When there's no cookie yet, we can't use the locale
+  // from the URL. Something like `require('next/navigation').usePathname()`
+  // would be great, but that uses non-server context currently, so not an
+  // option in server components. `params` is also not an option, since they are
+  // only available from matched segments downwards.
+  const lang = 'en';
+
   return (
-    <html lang="en">
+    <html lang={lang}>
       <head />
       <body>
         {children}
