9dc5ef019f73d7a058b19546673f93fd7d41e322
aXenDeveloper <axendeveloper@gmail.com>
2022-12-08 20:35:51 +0100

    Fix redirect path with _next dir

--- middleware.js
+++ middleware.js
@@ -17,7 +17,10 @@ export function middleware(req) {
   if (!lng) lng = fallbackLng
 
   // Redirect if lng in path is not supported
-  if (!languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`))) {
+  if (
+    !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) ||
+    !req.nextUrl.pathname.startsWith('/_next')
+  ) {
     return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
   }
 
