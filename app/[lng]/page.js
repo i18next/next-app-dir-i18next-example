fc059dd4efe57efc2788e27b9d673b61e3b87ce8
Adriano Raiano <adriano@raiano.ch>
2022-12-08 08:39:43 +0100

    remove noise

--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -18,43 +18,6 @@ export default async function Page({ params: { lng } }) {
             Welcome to Next.js v13 <small>appDir</small> and i18next
           </Trans>
         </h2>
-        {/* <div style={{ display: 'inline-flex', width: '90%' }}>
-          <div style={{ width: '50%' }}>
-            <h3 style={{ minHeight: 70 }}>
-              {t('blog.optimized.question')}
-            </h3>
-            <p>
-              <Trans i18n={i18n} i18nKey="blog.optimized.answer">
-                Then you may have a look at{' '}
-                <a href={t('blog.optimized.link')}>this blog post</a>
-                .
-              </Trans>
-            </p>
-            <a href={t('blog.optimized.link')}>
-              <img
-                style={{ width: '50%' }}
-                src="https://locize.com/blog/next-i18next/next-i18next.jpg"
-              />
-            </a>
-          </div>
-          <div style={{ width: '50%' }}>
-            <h3 style={{ minHeight: 70 }}>
-              {t('blog.ssg.question')}
-            </h3>
-            <p>
-              <Trans i18n={i18n} i18nKey="blog.ssg.answer">
-                Then you may have a look at{' '}
-                <a href={t('blog.ssg.link')}>this blog post</a>.
-              </Trans>
-            </p>
-            <a href={t('blog.ssg.link')}>
-              <img
-                style={{ width: '50%' }}
-                src="https://locize.com/blog/next-i18n-static/title.jpg"
-              />
-            </a>
-          </div>
-        </div> */}
         <hr style={{ marginTop: 20, width: '90%' }} />
         <div>
           <Link href={`/${lng}/second-page`}>
