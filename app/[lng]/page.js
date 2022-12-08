bdd4682649178e532555293020d39816d5afa8ae
Adriano Raiano <adriano@raiano.ch>
2022-12-08 17:05:32 +0100

    link to blog post

--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -18,6 +18,19 @@ export default async function Page({ params: { lng } }) {
             Welcome to Next.js v13 <small>appDir</small> and i18next
           </Trans>
         </h2>
+        <div style={{ width: '100%' }}>
+          <p>
+            <Trans i18nKey="blog.text">
+              Check out the corresponding <a href={t('blog.link')}>blog post</a> describing this example.
+            </Trans>
+          </p>
+          <a href={t('blog.link')}>
+            <img
+              style={{ width: '50%' }}
+              src="https://locize.com/blog/next-13-app-dir-i18n/next-13-app-dir-i18n.jpg"
+            />
+          </a>
+        </div>
         <hr style={{ marginTop: 20, width: '90%' }} />
         <div>
           <Link href={`/${lng}/second-page`}>
