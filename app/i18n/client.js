cfe77fbdb25408f183dd2839a14a745fd1ede8f4
Adriano Raiano <adriano@raiano.ch>
2022-12-15 11:09:21 +0100

    prevent hydration error #7

--- app/i18n/client.js
+++ app/i18n/client.js
@@ -4,14 +4,19 @@ import i18next from 'i18next'
 import { initReactI18next, useTranslation as useTranslationOrg } from 'react-i18next'
 import resourcesToBackend from 'i18next-resources-to-backend'
 // import LocizeBackend from 'i18next-locize-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { getOptions } from './settings'
 
 // on client side the normal singleton is ok
 i18next
   .use(initReactI18next)
+  .use(LanguageDetector)
   .use(resourcesToBackend((language, namespace) => import(`./locales/${language}/${namespace}.json`)))
   // .use(LocizeBackend) // locize backend could be used on client side, but prefer to keep it in sync with server side
-  .init(getOptions())
+  .init({
+    ...getOptions(),
+    lng: undefined // let detect the language on client side
+  })
 
 export function useTranslation(lng, ns, options) {
   if (i18next.resolvedLanguage !== lng) i18next.changeLanguage(lng)
