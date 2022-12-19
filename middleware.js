87f631f544319a7502d812cab1a1110e552fbf2a
Adriano Raiano <adriano@raiano.ch>
2022-12-19 09:35:09 +0100

    favicon

--- middleware.js
+++ middleware.js
@@ -11,6 +11,7 @@ export const config = {
 const cookieName = 'i18next'
 
 export function middleware(req) {
+  if (req.nextUrl.pathname.indexOf('icon') > -1 || req.nextUrl.pathname.indexOf('chrome') > -1) return NextResponse.next()
   let lng
   if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
