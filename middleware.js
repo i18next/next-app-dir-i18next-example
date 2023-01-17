a3763fc21e61004d839607f9e088c432c1d89569
Adriano Raiano <adriano@raiano.ch>
2023-01-17 13:45:30 +0100

    cosmetics

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ acceptLanguage.languages(languages)
 
 export const config = {
   // matcher: '/:lng*'
-  matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"]
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)']
 }
 
 const cookieName = 'i18next'
