850446686faf47aa899a4d8e123f265140f69152
Adriano Raiano <adriano@raiano.ch>
2023-11-01 07:15:09 +0100

    update deps

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
-  experimental: {
-    // Required:
-    appDir: true
-  }
+  // experimental: {
+  //   // Required: for next 13
+  //   appDir: true
+  // }
 }
 
 module.exports = nextConfig
