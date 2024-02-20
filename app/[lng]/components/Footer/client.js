17ba4b8e29a94c850c4debafb51c3e33af0a8fba
Adriano Raiano <adriano@raiano.ch>
2024-02-20 22:51:31 +0100

    hint: if you like to avoid prop drilling, you can do so with useParams() - #1725

--- app/[lng]/components/Footer/client.js
+++ app/[lng]/components/Footer/client.js
@@ -2,8 +2,16 @@
 
 import { FooterBase } from './FooterBase'
 import { useTranslation } from '../../../i18n/client'
+// import { useParams } from 'next/navigation'
 
 export const Footer = ({ lng, path }) => {
   const { t } = useTranslation(lng, 'footer')
   return <FooterBase t={t} lng={lng} path={path} />
 }
+
+// if you like to avoid prop drilling, you can do so with useParams()
+// export const Footer = ({ path }) => {
+//   const params = useParams()
+//   const { t } = useTranslation(params.lng, 'footer')
+//   return <FooterBase t={t} lng={params.lng} path={path} />
+// }
