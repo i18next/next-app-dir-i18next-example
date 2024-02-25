c5aab55f46098b95bf5e3ccf279fb63400c04c58
Adriano Raiano <adriano@raiano.ch>
2024-02-25 19:59:01 +0100

    fix generateMetadata

---
+++ app/[lng]/client-page/layout.js
@@ -0,0 +1,18 @@
+import { languages, fallbackLng } from '../../i18n/settings'
+import { useTranslation } from '../../i18n'
+
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }))
+}
+
+export async function generateMetadata({ params: { lng } }) {
+  if (languages.indexOf(lng) < 0) lng = fallbackLng
+  const { t } = await useTranslation(lng, 'client-page')
+  return {
+    title: t('title')
+  }
+}
+
+export default async function Layout({ children }) {
+  return children
+}
--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -1,10 +1,20 @@
 import './global.css'
 
 import { dir } from 'i18next'
-import { languages } from '../i18n/settings'
+import { languages, fallbackLng } from '../i18n/settings'
+import { useTranslation } from '../i18n'
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }))
+}
+
+export async function generateMetadata({ params: { lng } }) {
+  if (languages.indexOf(lng) < 0) lng = fallbackLng
+  const { t } = await useTranslation(lng)
+  return {
+    title: t('title'),
+    content: 'A playground to explore new Next.js 13/14 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching.'
+  }
 }
 
 export default function RootLayout({
